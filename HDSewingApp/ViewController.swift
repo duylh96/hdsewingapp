e73f6a3d89a1377fce3e95110f1943704ade9eed
Hoang Duy <hoangduy@duys-macbook-pro.local>
2018-05-04 10:25:31 +0700

    Initial Commit

---
+++ HDSewingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HDSewingApp
+//
+//  Created by Hoàng Duy on 5/4/18.
+//  Copyright © 2018 Hoàng Duy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
